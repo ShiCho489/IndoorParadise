228f2c936f7410797dd088b765294df615f332ce
Shivani Choudhary <choudhary.shivani489@gmail.com>
2022-12-09 14:11:10 +0200

    reactapp

---
+++ frontend/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import { Container } from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+function App() {
+  return (
+    <>
+
+    <Header />
+    <main>
+      <Container>
+       <h1> IndoorParadise</h1>
+      </Container>
+      </main>
+    
+      <Footer />
+      </>
+  );
+}
+
+export default App;
