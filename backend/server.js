022707373d12699421c679143e6897f5e24a805c
Shivani Choudhary <choudhary.shivani489@gmail.com>
2022-12-12 20:48:54 +0200

    updated

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js'
-import products from './data/products.js';
-
+import productRoutes from './routes/productRoutes.js'
 
 dotenv.config()
 
@@ -14,23 +13,13 @@ connectDB()
 const app = express();
 
 
-
-//routes middleware
-//app.use('/api', userRoutes);
-
 app.get('/', (req, res)=> {
     res.send('API is running....')
 })
 
-app.get('/api/products', (req, res)=> {
-    res.json(products)
-})
 
-app.get('/api/products/:id', (req, res)=> {
-    const product= products.find((p)=> p._id === req.params.id)
-    res.json(product)
-})
-
+//routes middleware
+app.use('/api/products', productRoutes);
 
 
 const port= process.env.PORT || 8000;
