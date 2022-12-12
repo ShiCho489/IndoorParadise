ceb5e867338068db7df30bccfbff13e100323fcf
Shivani Choudhary <choudhary.shivani489@gmail.com>
2022-12-12 14:22:05 +0200

    database connected

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,14 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import connectDB from './config/db.js'
 import products from './data/products.js';
 
 
 dotenv.config()
+
+
+//Databaseconnected
+connectDB()
 
 //app
 const app = express();
