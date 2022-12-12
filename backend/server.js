8168093699a31eafb81ee0870762f5d094b789ac
Shivani Choudhary <choudhary.shivani489@gmail.com>
2022-12-12 10:39:10 +0200

    backend server

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
 const express = require('express');
+const products = require('./data/products')
 const mongoose = require('mongoose');
 require('dotenv').config();
-//import routes
-const userRoutes = require('./routes/user')
+
 
 //app
 const app = express();
@@ -13,7 +13,20 @@ mongoose.connect(process.env.MONGO_URI, {
 }).then (() => console.log("DB is connected"));
 
 //routes middleware
-app.use('/api', userRoutes);
+//app.use('/api', userRoutes);
+
+app.get('/', (req, res)=> {
+    res.send('API is running..')
+})
+
+app.get('/api/products', (req, res)=> {
+    res.json(products)
+})
+
+app.get('/api/products/:id', (req, res)=> {
+    const product= products.find((p)=> p._id === req.params.id)
+    res.json(product)
+})
 
 
 
