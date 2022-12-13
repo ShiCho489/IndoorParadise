a8b2ef0f250f6d68cd257e1f1c2fbee70648055b
Shivani Choudhary <choudhary.shivani489@gmail.com>
2022-12-13 18:06:27 +0200

    Updates

---
+++ precache-manifest.d8485484bf8e432fa9e279beef7f7982.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2bc23e4ce97db3c6d725",
+    "url": "/IndoorParadise/static/css/main.80b682ce.chunk.css"
+  },
+  {
+    "revision": "2bc23e4ce97db3c6d725",
+    "url": "/IndoorParadise/static/js/main.2bc23e4c.chunk.js"
+  },
+  {
+    "revision": "70758bfbba5b42d58ad2",
+    "url": "/IndoorParadise/static/js/1.70758bfb.chunk.js"
+  },
+  {
+    "revision": "80a38260346666659581",
+    "url": "/IndoorParadise/static/js/runtime~main.80a38260.js"
+  },
+  {
+    "revision": "ab659e87681e6ba50d12cbe636d09e99",
+    "url": "/IndoorParadise/index.html"
+  }
+];
