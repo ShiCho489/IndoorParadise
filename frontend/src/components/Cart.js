dc98b32d69057a8f7509df4bf0821edb4b9349cc
Shivani Choudhary <choudhary.shivani489@gmail.com>
2022-12-13 10:00:52 +0200

    adding cart

---
+++ frontend/src/components/Cart.js
@@ -0,0 +1,8 @@
+import { useState, useEffect } from 'react'
+import { Button, Col, Form, Image, ListGroup, Row } from 'react-bootstrap'
+import { CartState } from '../context/productContext'
+import Rating from './Rating'
+
+const Cart =()=> {
+   
+}
